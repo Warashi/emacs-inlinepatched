4b90ff2f77ccf664a81de67c0af304e75c6d04a6
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-21 02:26:43 +0900

    fix

--- ensure_dependencies.sh
+++ ensure_dependencies.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xcode-select --install
-installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
