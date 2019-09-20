c2ab54bcf5ad5aab9bfa7aba0a60d273c7e81a01
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-21 02:35:19 +0900

    add deps

--- ensure_dependencies.sh
+++ ensure_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
-brew install autoconf
+brew install autoconf gnu-sed texinfo pkg-config gnutls
