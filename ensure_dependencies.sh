712068231595c185e0576cac5db85ec6472ed6df
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-24 22:42:07 +0900

    25.3のビルドにautomakeが必要っぽかった

--- ensure_dependencies.sh
+++ ensure_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
-brew install autoconf gnu-sed texinfo pkg-config gnutls
+brew install autoconf automake gnu-sed texinfo pkg-config gnutls
