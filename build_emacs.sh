d3e32ad632fccd46a0744412f0026877308f1770
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-24 21:19:13 +0900

    将来に備えてmatrix buildにして、tagでビルドするようにする

--- build_emacs.sh
+++ build_emacs.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 set -eu
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 version=$1
+os=$2
 
 curl -L -O "http://ftpmirror.gnu.org/emacs/emacs-${version}.tar.xz"
 tar -xf "emacs-${version}.tar.xz"
 cd "emacs-${version}"
 
-curl -L -O https://gist.githubusercontent.com/takaxp/3314a153f6d02d82ef1833638d338ecf/raw/156aaa50dc028ebb731521abaf423e751fd080de/emacs-25.2-inline.patch
-patch -p1 < emacs-25.2-inline.patch
+curl -s -L https://gist.githubusercontent.com/takaxp/3314a153f6d02d82ef1833638d338ecf/raw/156aaa50dc028ebb731521abaf423e751fd080de/emacs-25.2-inline.patch | patch -p1
 
 ./autogen.sh
-./configure --with-modules --with-ns "--enable-locallisppath=/Library/Application Support/Emacs/${version}/site-lisp:/Library/Application Support/Emacs/site-lisp" '--datarootdir=/Applications/Emacs.app/Contents/Resources'
+./configure --with-modules --with-ns "--enable-locallisppath=/Library/Application Support/Emacs/${version}/site-lisp:/Library/Application Support/Emacs/site-lisp"
 make -j
 make install
+$script_dir/copylib.rb ./nextstep/Emacs.app/Contents/MacOS/Emacs /usr/local nextstep/Emacs.app/Contents/MacOS/lib
+cp $script_dir/launch.sh ./nextstep/Emacs.app/Contents/MacOS/Emacs.sh
 
 mkdir -p $HOME/dmg
-ln -s /Applications $HOME/dmg/Applicatons
-mv nextstep/Emacs.app $HOME/dmg/
+ln -sf /Applications $HOME/dmg/Applicatons
+mv ./nextstep/Emacs.app $HOME/dmg/
 
 mkdir -p ../dist
-hdiutil create ../dist/Emacs-mojave.dmg -volname "Emacs" -srcfolder $HOME/dmg
+hdiutil create ../dist/Emacs-$os.dmg -volname "Emacs" -srcfolder $HOME/dmg
