8b7feedb5b1e1f40a8fbbe1a2da3fb8ba82f02d5
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-27 00:30:59 +0900

    gnu-sedが使えてないのになんでビルド通ってたんだろう？

--- build_emacs.sh
+++ build_emacs.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 EMACS_VERSION=${1}
 OS_VERSION="$(sw_vers -productVersion | cut -f '1,2' -d '.')"
+export PATH="$(brew --prefix gnu-sed)/libexec/gnubin:$PATH"
 
 curl -L -O "http://ftpmirror.gnu.org/emacs/emacs-${EMACS_VERSION}.tar.xz"
 tar -xf "emacs-${EMACS_VERSION}.tar.xz"
