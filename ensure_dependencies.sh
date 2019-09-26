469b02dcece8d3e11079fade5d3b698ebfb81ad2
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-27 00:05:03 +0900

    依存しているヘッダのインストールが10.14専用になっていたのを修正

--- ensure_dependencies.sh
+++ ensure_dependencies.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
-sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+#!/bin/bash
+set -euo pipefail
+OS_VERSION="$(sw_vers -productVersion | cut -f '1,2' -d '.')"
+sudo installer -pkg "/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_${OS_VERSION}.pkg" -target /
 brew install autoconf automake gnu-sed texinfo pkg-config gnutls
