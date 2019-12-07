aff28fae7216027a9a0a211ed55f40d4a2cbdd89
Shinnosuke Sawada <6warashi9@gmail.com>
2019-12-07 15:25:50 +0900

    macOS-Catalinaでビルドとかするようにした

--- ensure_dependencies.sh
+++ ensure_dependencies.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -euo pipefail
-OS_VERSION="$(sw_vers -productVersion | cut -f '1,2' -d '.')"
-sudo installer -pkg "/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_${OS_VERSION}.pkg" -target /
 brew install autoconf automake gnu-sed texinfo pkg-config gnutls
